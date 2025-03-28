cc535ce3ba12e45fec72f4ba0893da58b850017e
Maria Margarida <mmargarida688rgmail.com>
2025-03-28 13:49:32 +0000

    Add and create project Chronos in solution LP1Semana06.

---
+++ Chronos/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+
+namespace Chronos
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Stopwatch crono1 = new Stopwatch();
+            Stopwatch crono2 = new Stopwatch();
+
+            crono1.Start();
+            Thread.Sleep(500); 
+            crono1.Stop();
+
+            crono2.Start();
+            Thread.Sleep(250);
+            crono2.Stop();
+
+
+            Console.WriteLine($"{crono1.ElapsedMilliseconds / 1000.0}");
+            Console.WriteLine($"{crono2.ElapsedMilliseconds / 1000.0}");
+
+            Console.ReadKey();
+        }
+    }
+}
+
+
+
